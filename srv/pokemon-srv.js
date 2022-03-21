d8e19288c65f2588c46f503a8f0074d9bb82c486
Sara Moreno <sara.moreno.da.silva@everis.nttdata.com>
2022-03-21 11:57:50 +0000

    merge

--- srv/pokemon-srv.js
+++ srv/pokemon-srv.js
@@ -1,17 +1,26 @@
 const cds = require('@sap/cds')
-module.exports = function (){
-  this.before('CREATE', 'entrenadores',(req)=>{
-    req.data.level = 1;
-  });
-  this.on('READ', 'generation', async (req) =>{
-      let sQuery;
-    const pokedexApi = await cds.connect.to("pokedex");
-    if (req.data.id) {
-        sQuery = "/generation/" + req.data.id;
-    }else{
-        sQuery = "/generation/";
-    }
-      const generation = await pokedexApi.tx(req).get(sQuery);
-    return generation;
-  });
+module.exports = function () {
+    //const { ciudades } = this.entities;
+    this.before('CREATE', 'entrenadores', (req) => {
+        req.data.level = 1;
+        /*if (!req.data.level) {
+            req.error (409,`Level is not informed`);
+        }*/
+    });
+    this.on('READ', 'generation', async (req) => {
+        let sQuery;
+        const pokedexApi = await cds.connect.to("pokedex");
+        if (req.data.id) {
+            sQuery = "/generation/" + req.data.id;
+        } else {
+            sQuery = "/generation/";
+        }
+        const generation = await pokedexApi.tx(req).get(sQuery);
+        return generation;
+    });
+    this.on('getAllCities', async (req) => {
+        const tx = cds.transaction(req);
+        let cities = await tx.run(SELECT.from('ciudades'));
+        return cities;
+    });
 }
